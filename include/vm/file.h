8e59f2e37badee2259a8fe058ed048fd60f6f728
Minkyu Jung <hestati@kaist.ac.kr>
2020-12-31 18:57:04 +0900

    correct the term

--- include/vm/file.h
+++ include/vm/file.h
@@ -10,7 +10,7 @@ struct file_page {
 };
 
 void vm_file_init (void);
-bool file_map_initializer (struct page *page, enum vm_type type, void *kva);
+bool file_backed_initializer (struct page *page, enum vm_type type, void *kva);
 void *do_mmap(void *addr, size_t length, int writable,
 		struct file *file, off_t offset);
 void do_munmap (void *va);
--- vm/file.c
+++ vm/file.c
@@ -1,16 +1,16 @@
-/* file.c: Implementation of memory mapped file object (mmaped object). */
+/* file.c: Implementation of memory backed file object (mmaped object). */
 
 #include "vm/vm.h"
 
-static bool file_map_swap_in (struct page *page, void *kva);
-static bool file_map_swap_out (struct page *page);
-static void file_map_destroy (struct page *page);
+static bool file_backed_swap_in (struct page *page, void *kva);
+static bool file_backed_swap_out (struct page *page);
+static void file_backed_destroy (struct page *page);
 
 /* DO NOT MODIFY this struct */
 static const struct page_operations file_ops = {
-	.swap_in = file_map_swap_in,
-	.swap_out = file_map_swap_out,
-	.destroy = file_map_destroy,
+	.swap_in = file_backed_swap_in,
+	.swap_out = file_backed_swap_out,
+	.destroy = file_backed_destroy,
 	.type = VM_FILE,
 };
 
@@ -19,9 +19,9 @@ void
 vm_file_init (void) {
 }
 
-/* Initialize the file mapped page */
+/* Initialize the file backed page */
 bool
-file_map_initializer (struct page *page, enum vm_type type, void *kva) {
+file_backed_initializer (struct page *page, enum vm_type type, void *kva) {
 	/* Set up the handler */
 	page->operations = &file_ops;
 
@@ -30,19 +30,19 @@ file_map_initializer (struct page *page, enum vm_type type, void *kva) {
 
 /* Swap in the page by read contents from the file. */
 static bool
-file_map_swap_in (struct page *page, void *kva) {
+file_backed_swap_in (struct page *page, void *kva) {
 	struct file_page *file_page UNUSED = &page->file;
 }
 
 /* Swap out the page by writeback contents to the file. */
 static bool
-file_map_swap_out (struct page *page) {
+file_backed_swap_out (struct page *page) {
 	struct file_page *file_page UNUSED = &page->file;
 }
 
-/* Destory the file mapped page. PAGE will be freed by the caller. */
+/* Destory the file backed page. PAGE will be freed by the caller. */
 static void
-file_map_destroy (struct page *page) {
+file_backed_destroy (struct page *page) {
 	struct file_page *file_page UNUSED = &page->file;
 }
 

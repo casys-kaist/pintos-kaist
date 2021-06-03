23025b90d78dde4e6b5b687a53b582b97c2a8264
KimDongok <ehddhr4454@kaist.ac.kr>
2021-06-03 00:25:30 -0700

    Add fs disk inspector

--- include/lib/user/syscall.h
+++ include/lib/user/syscall.h
@@ -58,4 +58,24 @@ static inline void* get_phys_addr (void *user_addr) {
 	return pa;
 }
 
+static inline long long
+get_fs_disk_read_cnt (void) {
+	long long read_cnt;
+	asm volatile ("movq $0, %rdx");
+	asm volatile ("movq $1, %rcx");
+	asm volatile ("int $0x43");
+	asm volatile ("\t movq %%rax, %0": "=r" (read_cnt));
+	return read_cnt;
+}
+
+static inline long long
+get_fs_disk_write_cnt (void) {
+	long long write_cnt;
+	asm volatile ("movq $0, %rdx");
+	asm volatile ("movq $1, %rcx");
+	asm volatile ("int $0x44");
+	asm volatile ("\t movq %%rax, %0": "=r" (write_cnt));
+	return write_cnt;
+}
+
 #endif /* lib/user/syscall.h */

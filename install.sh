93ecff98aa52480b1d3997a96da106b9a5dfa36f
sjchoi <sjchoi@casys.kaist.ac.kr>
2022-03-07 08:16:16 +0000

    fixed install script to run on ubuntu 16.04 STANDARD-1 KCLOUD VM

--- install.sh
+++ install.sh
@@ -1,19 +1,23 @@
 
 #!/bin/bash
-# Tested on Ubuntu 18.04.1 LTS
+# Tested on Ubuntu 16.04 (STANDARD-1 KCLOUD VM)
 set -ex
 
 sudo apt-get -y update
 
 # gcc
-sudo apt-get -y install gcc=4:7.4.0-1ubuntu2.3
+sudo apt-get install -y software-properties-common python-software-properties
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt update && sudo apt install g++-7 -y
+
+#sudo apt-get -y install gcc=4:7.4.0-1ubuntu2.3
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 30
 sudo update-alternatives --set gcc /usr/bin/gcc-7
 gcc --version | grep "7.5.0-3ubuntu1~18.04"
 
 # qemu
-sudo apt-get -y install qemu=1:2.11+dfsg-1ubuntu7.36
-qemu-x86_64 --version | grep "1:2.11+dfsg-1ubuntu7.36"
+sudo apt-get -y install qemu=1:2.5+dfsg-5ubuntu10.51
+qemu-x86_64 --version | grep "1:2.5+dfsg-5ubuntu10.51"
 
 # make
 sudo apt-get -y install make

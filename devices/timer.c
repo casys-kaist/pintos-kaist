cad550141d476b5aa998fe16d2f0902a216c1950
DreamerKJ <meadowdreamer1@gmail.com>
2024-03-11 15:38:22 +0900

    fix: pass ticks to thread_wake_iter

--- devices/timer.c
+++ devices/timer.c
@@ -123,7 +123,7 @@ static void
 timer_interrupt (struct intr_frame *args UNUSED) {
 	ticks++;
 	thread_tick ();
-	thread_wake_iter();
+	thread_wake_iter(ticks);
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer

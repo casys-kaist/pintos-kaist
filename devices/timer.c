0ad608d1362066c9f2e4d5934d4b6ebc5aad4d22
Wonbin-Jung <santabin0222@gmail.com>
2024-10-02 18:11:54 +0000

    Change timer_interrupt

--- devices/timer.c
+++ devices/timer.c
@@ -125,6 +125,20 @@ static void
 timer_interrupt (struct intr_frame *args UNUSED) {
 	ticks++;
 	thread_tick ();
+	
+	if (thread_mlfqs) {
+		mlfqs_increment_recent_cpu ();
+
+		if (ticks % 4 == 0) {
+			mlfqs_recalculate_priority ();
+		}
+		
+		if (ticks % TIMER_FREQ == 0) {
+			mlfqs_update_load_avg ();
+			mlfqs_recalculate_recent_cpu ();
+		}
+	}
+
 	thread_check_wakeup_time (ticks);
 }
 
